a681ccdb96bd2eee775250822030a636747424d7
Paul Alexander <otulajapaul@outlook.com>
2022-11-20 12:29:54 +0100

    model deployed

--- covid19_2.py
+++ covid19_2.py
@@ -41,7 +41,7 @@ from sklearn.model_selection import train_test_split
 trainX, testX, trainY, testY = train_test_split(X,y, test_size = .2, random_state= 123)
 
 #%% The model fn
-def trn(df_train, y_train):
+def train(df_train, y_train):
     # Dictionary vectorizer
     dicts = df_train.to_dict(orient = 'records')
     dv = DictVectorizer(sparse=False)
@@ -55,7 +55,7 @@ def trn(df_train, y_train):
     return dv, model
 
 # Prediction function
-def pred(df, dv, model):
+def predict(df, dv, model):
     dicts = df.to_dict(orient = 'records')
     X = dv.transform(dicts)
 
@@ -64,10 +64,10 @@ def pred(df, dv, model):
 
 
 #%%
-dv, model = trn(trainX, trainY)
+dv, model = train(trainX, trainY)
 
 # %% Prediction and accuracy
-pred_y = pred(testX, dv, model)
+pred_y = predict(testX, dv, model)
 accuracy_score(testY, pred_y)
 
 
@@ -81,7 +81,47 @@ with open(output_file, 'wb') as f_out:
 
 #%% Sample individual data
 patient = testX.iloc[3].to_dict()
+
 # %%
-patient = dv.transform(patient)
-model.predict(patient)
+X_dv = dv.transform(patient)
+model.predict(X_dv)
+
+#%% Probability
+model.predict_proba(X_dv)[0,1]
+
+
+# %%
+# Creating Patient's blood test result as input
+ipatient = {'Age': 53.0,
+    'Hematocrit': 41.7,
+    'HCT 35-45%': 1.0,
+    'HGB': 14.3,
+    'HGB 12-15.5 g/dl': 0.0,
+    'MCH': 29.7,
+    'MCH 26-34 pg': 0.0,
+    'MCHC': 34.3,
+    'MCHC 31-36 g/dl': 0.0,
+    'MCV': 86.5,
+    'MCV 82-98 fl': 0.0,
+    'RBC Count': 4.82,
+    'RBC Count 3.9 - 5.0 x10^6/uL': 0.0,
+    'RDW': 12.2,
+    'RDW 11.5 - 16.5%': 0.0,
+    'Total WBC Count': 4.31,
+    'WBC Count 3.5-10.5 x10^3/uL': 0.0,
+    'Lymphocyte ': 1500.0,
+    'Lymphocyte 900 - 2900 ?': 0.0,
+    'Basophils ': 30.0,
+    'Basophil 0 - 100 ?': 0.0,
+    'Eosinphils ': 5.1,
+    'Eosinphils 50 - 500 uL': 1.0,
+    'Neutrophil ': 2142.0,
+    'Neutrophils 1700 - 8000 ?': 0.0,
+    'Monocytes ': 409.0,
+    'Monocytes 300 - 900 ?': 0.0,
+    'Plt Counts': 225.0,
+    'Platelet 150-450 x10^3/uL   ': 0.0,
+    'Patient_Sex_F': 0.0,
+    'Patient_Sex_M': 1.0
+    }
 # %%
--- deploy.py
+++ deploy.py
@@ -1,8 +1,26 @@
-#%%
+
 import pickle
+from flask import Flask, request, jsonify
 
 with open('./model.bin', 'rb') as f_in:
     (dv, model) = pickle.load(f_in)
 
 
-pred()
+
+app = Flask('covid19')
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+
+    patient = request.get_json()    
+
+    X_dv = dv.transform(patient)
+    y_pred = model.predict(X_dv)
+    result = {
+        'Is covid detected?': y_pred
+    }
+
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug = True, host = '0.0.0.0', port = 5353)
